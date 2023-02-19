58dda56bfc7927c2cdff2956cc495667d57ae26e
abdelkarim <abdelkarimbenchelef.1361@gmail.com>
2023-02-19 23:10:38 +0100

    init project ...

---
+++ src/components/Forms.js
@@ -0,0 +1,12 @@
+export const Forms = () =>{
+    return(
+        <form>
+            <input type="text" placeholder="Full Name ..." />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Age ..." />
+            <input type="password" placeholder="Password..." />
+            <input type="password" placeholder="Confirm password..." />
+            <input type="submit" />
+        </form>
+    )
+}
